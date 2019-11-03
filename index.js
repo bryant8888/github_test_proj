7aebbfe276a411a27068a1197ed24654db7da00b
bryant8888 <javacenter2013@gmail.com>
2019-11-03 13:13:37 +0300

    proj03 - create sum function

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-var userName = prompt('Enter user name');
+// var userName = prompt('Enter user name');
+//
+// var secondName = prompt('Enter your second name');
+//
+// console.log(`Hello, ${userName} ${secondName}!`);
 
-var secondName = prompt('Enter your second name');
 
-console.log(`Hello, ${userName} ${secondName}!`);
+let firstNumber = prompt('Введите первое число');
+let secondNumber = prompt('Введите второе число');
+
+const sumNumber = (firstNumber, secondNumber) => {
+    return alert(Number(firstNumber) + Number(secondNumber))
+}
+
+sumNumber(firstNumber, secondNumber)
